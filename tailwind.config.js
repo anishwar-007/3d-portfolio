87f7cfa50eeeb04def598736557336cc548628af
anishwar-007 <sharmaanish7779@gmail.com>
2023-06-24 20:34:51 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ export const theme = {
   extend: {
     colors: {
       primary: "#0a1130",
-      secondary: "#0b1540",
-      tertiary: "#151030",
+      secondary: "#aaa6c3",
+      tertiary: "#0b1540",
       "black-100": "#100d25",
       "black-200": "#090325",
       "white-100": "#f3f3f3",
